ea4d60460b92ec6ed6a312dc837069748d266a08
KNyathi <khayelihlenyathi07@gmail.com>
2024-01-20 11:41:26 +0300

    Setting up the CRUD endpoints for User and Tweet for our social media app

---
+++ backend/src/routes/tweetRoutes.ts
@@ -0,0 +1,39 @@
+import {Router} from 'express';
+
+const router = Router();
+
+
+//User CRUD endpoints defined here
+
+//Create a tweet
+router.post('/', (req, res) => {
+	res.status(501).json({ error: 'Not implemented' });
+});
+
+
+//List tweets
+router.get('/', (req, res) => {
+	res.status(501).json({ error: 'Not implemented' });
+});
+
+
+//get one tweet
+router.get('/:id', (req, res) => {
+	const { id } =  req.params;
+	res.status(501).json({ error: 'Not implemented: ${id}' });
+});
+
+//update tweet
+router.put('/:id', (req, res) => {
+	const { id } =  req.params;
+	res.status(501).json({ error: 'Not implemented: ${id}' });
+});
+
+//delete tweet
+router.delete('/:id', (req, res) => {
+	const { id } =  req.params;
+	res.status(501).json({ error: 'Not implemented: ${id}' });
+});
+
+export default router;
+
