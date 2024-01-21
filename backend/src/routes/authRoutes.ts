cd9f9f2f365c58d977b42424c9c58201e6ba16b3
KNyathi <khayelihlenyathi07@gmail.com>
2024-01-21 17:08:52 +0300

    to work on email sending

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 
 const EMAIL_TOKEN_EXPIRATION_MINUTES = 10;
 const API_TOKEN_EXPIRATION_HOURS = 12; //keep users logged in for twelve hours (then they can go to sleep hahaha)
-const JWT_SECRET = "SUPER SECRET";
+const JWT_SECRET = process.env.JWT_SECRET || 'SUPER SECRET'; 
 
 const router = Router();
 const prisma = new PrismaClient();
---
+++ backend/src/services/emailService.ts
@@ -0,0 +1,5 @@
+export async function sendEmailToken(email: string, token: string) {
+	
+}
+
+sendEmailToken('khayelihlenyathi07@gmail.com', '123');
