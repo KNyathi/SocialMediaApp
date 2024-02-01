f265164d45e064157551f59a6ae2965baa5e0070
KNyathi <khayelihlenyathi07@gmail.com>
2024-02-01 19:05:56 +0300

    SES emails

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -2,33 +2,33 @@ import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 import { error } from 'console';
 require('dotenv').config()
 
-const ses = new SESClient({});
+const ses = new SESClient({ region: 'us-east-1'});
 
-function createSendEmailCommand(toAddress: string, fromAddress: string) {
-   return new SendEmailCommand ({
+function createSendEmailCommand(toAddress: string, fromAddress: string, message: string) {
+   return new SendEmailCommand({
       Destination: {
         ToAddresses: [toAddress],
       },
       Source: fromAddress,
       Message: {
         Subject: {
-          CharSet: 'UTF-8',
-          Data: "Your one-time password"
+          Charset: 'UTF-8',
+          Data: "Your one-time password",
         },
         Body: {
           Text: {
-            CharSet: 'UTF-8',
-            Data: message
+            Charset: 'UTF-8',
+            Data: message,
           },
         },
         
-      }
-   })
+      },
+   });
 }
 
 export async function sendEmailToken(email: string, token: string) {
    console.log('email: ', email, token);
-   const message = `Your one time password: ${token}` 
+   const message = `Your one time password: ${token}`; 
    const command = createSendEmailCommand(email, "khayelihlenyathi07@gmail.com", message);
 
    try {
@@ -39,4 +39,4 @@ export async function sendEmailToken(email: string, token: string) {
    }
 }
 
-sendEmailToken('khayelihlenyathi07@gmail.com', '123');
+
